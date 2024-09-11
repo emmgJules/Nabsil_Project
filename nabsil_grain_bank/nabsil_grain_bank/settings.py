68b773a5f74abeeed05bcbdf3b81d2ddebc0b17d
the-map003 <aimepatrick2003@gmail.com>
2024-09-11 18:35:43 +0200

    Upload

--- nabsil_grain_bank/nabsil_grain_bank/settings.py
+++ nabsil_grain_bank/nabsil_grain_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-n#b7(*e*(r*mcir+r#*4n$n^)g@!s5a%t4kil%1*gpego_ogxr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +123,9 @@ STATICFILES_DIRS = [
             BASE_DIR /'static'
 
      ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
