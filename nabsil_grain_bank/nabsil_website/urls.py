da7d08fc9ce8b2523c317b1a742f1d32ffaa90e3
the-map003 <aimepatrick2003@gmail.com>
2024-09-12 19:48:10 +0200

    Last Changes

--- nabsil_grain_bank/nabsil_website/urls.py
+++ nabsil_grain_bank/nabsil_website/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index, name="index_page"),
+    path('faqs',views.faqs, name="faqs"),
+    path('contact',views.contact, name="contact"),
+    path('blog',views.blog, name="blog"),
 ]
--- nabsil_grain_bank/nabsil_website/views.py
+++ nabsil_grain_bank/nabsil_website/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from datetime import datetime
 
 def index(request):
-    return render(request, 'nabsil_website/index.html')
+    current_year = datetime.now().year
+    return render(request, 'nabsil_website/index.html', {'current_year': current_year})
 
+def contact(request):
+    current_year = datetime.now().year
+    return render(request, 'nabsil_website/contact/contact.html', {'current_year': current_year})
+
+def faqs(request):
+    current_year = datetime.now().year
+    return render(request, 'nabsil_website/faqs/faqs.html', {'current_year': current_year})
+
+def blog(request):
+    current_year = datetime.now().year
+    return render(request, 'nabsil_website/blog/blog.html', {'current_year': current_year})
